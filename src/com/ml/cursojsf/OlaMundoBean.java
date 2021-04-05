706b78063a454cba362cc048d36fa5d410060446
mlima3696 <mjuniorlima3@gmail.com>
2021-04-04 23:41:54 -0300

    Managed-Beans

---
+++ src/com/ml/cursojsf/OlaMundoBean.java
@@ -0,0 +1,21 @@
+package com.ml.cursojsf;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class OlaMundoBean {
+
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void enviar() {
+		this.setNome(this.getNome().toUpperCase());
+	}
+}
